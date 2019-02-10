db28d7874de5383eb54f07b99f8632da23b633b7
Jeff Lane <xpresslanej@gmail.com>
2019-02-10 00:03:30 -0800

    Added Off API

--- tracer.go
+++ tracer.go
@@ -14,12 +14,21 @@ type tracer struct {
 	out io.Writer
 }
 
+type nilTracer struct{}
+
 //New returns address of new tracer
 func New(w io.Writer) Tracer {
 	return &tracer{out: w}
+}
+
+//Off API to stop/prevent tracing
+func Off() Tracer {
+	return &nilTracer{}
 }
 
 func (t *tracer) Trace(a ...interface{}) {
 	fmt.Fprint(t.out, a...)
 	fmt.Fprintln(t.out)
 }
+
+func (t *nilTracer) Trace(a ...interface{}) {}
